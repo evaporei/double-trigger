4cc9700611458a4d86cfd542722a2e42b5a38871
Eva Pace <eba.pachi@gmail.com>
2022-10-03 16:01:32 -0500

    Initial commit

---
+++ tests/contract.test.ts
@@ -0,0 +1,72 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { ExampleEntity } from "../generated/schema"
+import { Pregnant } from "../generated/Contract/Contract"
+import { handlePregnant } from "../src/contract"
+import { createPregnantEvent } from "./contract-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let owner = Address.fromString("0x0000000000000000000000000000000000000001")
+    let matronId = BigInt.fromI32(234)
+    let sireId = BigInt.fromI32(234)
+    let cooldownEndBlock = BigInt.fromI32(234)
+    let newPregnantEvent = createPregnantEvent(
+      owner,
+      matronId,
+      sireId,
+      cooldownEndBlock
+    )
+    handlePregnant(newPregnantEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("ExampleEntity created and stored", () => {
+    assert.entityCount("ExampleEntity", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "owner",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "matronId",
+      "234"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "sireId",
+      "234"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "cooldownEndBlock",
+      "234"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
